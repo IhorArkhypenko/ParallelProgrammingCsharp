7b320bd86d1fc3fda8119cb870aa8f030e520a7b
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2024-10-24 13:07:11 +0300

    Ways to create a Task.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -2,8 +2,27 @@
 
 class Program
 {
+    public static void Write(char c)
+    {
+        int i = 1000;
+        while (i-- > 0)
+        {
+            Console.Write(c);
+        }
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // First way of creating and starting Task at the same time.
+        Task.Factory.StartNew(() => Write('1'));
+        
+        // Second way of creating Task without starting it.
+        var task = new Task(() => Write('2'));
+        task.Start(); // Starting of the second Task in other thread.
+        
+        Write('3'); // Operation in the main thread.
+        
+        Console.WriteLine("Main program done.");
+        Console.ReadKey();
     }
 }
