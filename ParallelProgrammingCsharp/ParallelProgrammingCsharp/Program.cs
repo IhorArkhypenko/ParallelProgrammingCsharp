4b8c190e67531fd6d7770aa4eb60267b99c3e35e
Ihor Akhypenko <wasbornindecember@gmail.com>
2025-02-23 22:59:59 +0200

    Way to cancel executing task.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -2,22 +2,26 @@
 
 class Program
 {
-    static int TextLengh(object? o)
-    {
-        Console.WriteLine($"Task {Task.CurrentId} processing object {o}...");
-        return o.ToString().Length;
-    }
-    
     static void Main(string[] args)
     {
-        string text1 = "test";
-        string text2 = "testing";
-        
-        var task1 = Task.Factory.StartNew(TextLengh, text1);
-        var task2 = new Task<int>(TextLengh, text2);
-        task2.Start();
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
 
-        Console.WriteLine($"Length of {text1} is {task1.Result}");
-        Console.WriteLine($"Length of {text2} is {task2.Result}");
+        var task = new Task(() =>
+        {
+            var i = 0;
+            while (true)
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                Console.WriteLine($"{i++} \t");
+            }
+        }, token);
+
+        task.Start();
+
+        Console.ReadKey();
+        cts.Cancel();
+
+        Console.ReadKey();
     }
 }
