8e4f4f3713ce7f5aa911beff3e800af09aac6b9c
Ihor Akhypenko <wasbornindecember@gmail.com>
2025-02-23 21:57:47 +0200

    Code cleaned. I am going to start from scratch.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -2,32 +2,8 @@
 
 class Program
 {
-    static void Write(char c)
-    {
-        int i = 1000;
-        while (i-- > 0)
-        {
-            Console.Write(c);
-        }
-    }
-
-    static void Write(object obj)
-    {
-        int i = 1000;
-        while (i-- > 0)
-        {
-            Console.Write(obj);
-        }
-    }
-    
     static void Main(string[] args)
     {
-        Task.Factory.StartNew(Write, "hello");
 
-        var task = new Task(Write, 123);
-        task.Start();
-
-        Console.WriteLine("Main program done.");
-        Console.ReadKey();
     }
 }
