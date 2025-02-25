f84b27b794181044f68b5202a320880d7b99b426
Ihor Akhypenko <wasbornindecember@gmail.com>
2025-02-26 00:29:50 +0200

    Example of handling AggregateException.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -4,38 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        var cts = new CancellationTokenSource();
-        var token = cts.Token;
+        var task1 = Task.Factory.StartNew(() => throw new InvalidOperationException("Can't do this!") { Source = "task1" });
+        var task2 = Task.Factory.StartNew(() => throw new AccessViolationException() { Source = "task2" });
 
-        var task = new Task(() =>
+        try
         {
-            Console.WriteLine("I take 5 seconds.");
-
-            for (int i = 0; i < 5; i++)
+            Task.WaitAll(task1, task2);
+        }
+        catch (AggregateException ae)
+        {
+            foreach (var exception in ae.InnerExceptions)
             {
-                token.ThrowIfCancellationRequested();
-                Thread.Sleep(1000);
+                Console.WriteLine($"Exception: {exception.GetType()} from {exception.Source}");
             }
-
-            Console.WriteLine("I am done!");
-        }, token);
-        task.Start();
-
-        var task2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
-
-        // Will wait for all pending tasks.
-        // Task.WaitAll(task, task2); 
-
-        // Wait for particular task.
-        // task.Wait(token);
-
-        // Will wait for first completed task. 4000 is ms timeout(same available in WaitAll).
-        // Task.WaitAny(new[] {task, task2}, 4000, token); 
-
-        Console.WriteLine($"task status is {task.Status}");
-        Console.WriteLine($"task2 status is {task2.Status}");
-
-        Console.WriteLine("Main program done!");
-        Console.ReadKey();
+        }
     }
 }
