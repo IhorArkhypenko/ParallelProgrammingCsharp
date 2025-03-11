a0ba041677201e6f80ee02b27f773167309af2ba
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-03-11 17:52:39 +0200

    ConcurrentDictionaryExample Added.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using ParallelProgrammingCsharp.DataSharingAndSynchronization;
+﻿using ParallelProgrammingCsharp.ConcurrentCollections;
+using ParallelProgrammingCsharp.DataSharingAndSynchronization;
 using ParallelProgrammingCsharp.TaskProgramming;
 using ParallelProgrammingCsharp.TaskProgramming.Cancellation;
 using ParallelProgrammingCsharp.TaskProgramming.ExceprionHandling;
@@ -25,5 +26,8 @@ class Program
         // SingleMutexExample.Run();
         // MultipleMutexExample.Run();
         // ReaderWriterLockExample.Run();
+
+        // Concurrent collections.
+        // ConcurrentDictionaryExample.Run();
     }
 }
