6f9e5dd6884fd77ca9a7a4027c17c3bbbf369461
Ihor Akhypenko <wasbornindecember@gmail.com>
2025-03-16 17:06:53 +0200

    ContinuationsExample added.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ParallelProgrammingCsharp.DataSharingAndSynchronization;
+using ParallelProgrammingCsharp.TaskCoordination;
 using ParallelProgrammingCsharp.TaskProgramming;
 using ParallelProgrammingCsharp.TaskProgramming.Cancellation;
 using ParallelProgrammingCsharp.TaskProgramming.ExceprionHandling;
@@ -25,5 +26,8 @@ class Program
         // SingleMutexExample.Run();
         // MultipleMutexExample.Run();
         // ReaderWriterLockExample.Run();
+
+        // Task coordination.
+        // ContinuationsExample.Run();
     }
 }
---
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/TaskCoordination/ContinuationsExample.cs
@@ -0,0 +1,38 @@
+﻿namespace ParallelProgrammingCsharp.TaskCoordination;
+
+public class ContinuationsExample
+{
+    public static void Run()
+    {
+        // Way to start one task after another.
+        var task = Task.Factory.StartNew(() =>
+        {
+            Console.WriteLine("Boiling water!");
+        });
+
+        var task2 = task.ContinueWith(t =>
+        {
+            Console.WriteLine($"Water boiled by task {t.Id}. Ready to serve in cups!");
+        });
+
+        task2.Wait();
+
+        // Way to continue after more than one task will be completed. (Also method ContinueWithAny() exists.)
+        var t = Task.Factory.StartNew(() => "Task 1");
+        var t2 = Task.Factory.StartNew(() => "Task 2");
+
+        var t3 = Task.Factory.ContinueWhenAll(
+            new[] { t, t2 },
+            tasks =>
+        {
+            Console.WriteLine("Tasks completed:");
+            foreach (var item in tasks)
+            {
+                Console.WriteLine("- " + item.Result);
+            }
+            Console.WriteLine("All tasks done.");
+        });
+
+        t3.Wait();
+    }
+}
