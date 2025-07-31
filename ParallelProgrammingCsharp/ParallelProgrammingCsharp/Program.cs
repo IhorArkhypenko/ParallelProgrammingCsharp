58587469a25ecc3539911082564f3b4219679644
Ihor Arkhypenko <iarkhypenko@teamworkcommerce.com>
2025-07-31 14:38:53 +0300

    Formatting changed.

--- ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
+++ ParallelProgrammingCsharp/ParallelProgrammingCsharp/Program.cs
@@ -1,3 +1,9 @@
+using ParallelProgrammingCsharp._1.TaskProgramming;
+using ParallelProgrammingCsharp._1.TaskProgramming.Cancellation;
+using ParallelProgrammingCsharp._1.TaskProgramming.ExceprionHandling;
+using ParallelProgrammingCsharp._2.DataSharingAndSynchronization;
+using ParallelProgrammingCsharp._3.ConcurrentCollections;
+using ParallelProgrammingCsharp._4.TaskCoordination;
 using ParallelProgrammingCsharp._5.ParallelLoops;
 using ParallelProgrammingCsharp._6.ParallelLinq;
 
@@ -10,44 +16,44 @@ public class Program
         // Uncomment line of code you want to run.
 
         // DeadLockExample.Run();
-
-        // Task programming.
+        //
+        // // Task programming.
         // CreateAndRunTaskExample.Run();
         // TaskCancellationExample.Run();
         // LinkedTokenSourceExample.Run();
         // WaitForATaskCompletionExample.Run(); // Need to uncomment one way to wait in method.
         // TaskExceptionHandlingExample.Run();
         // TaskAggregateExceptionHandleExample.Run();
-
-        // Data sharing and synchronization.
+        //
+        // // Data sharing and synchronization.
         // CriticalSectionLockExample.Run();
         // InterlockedExample.Run();
         // SingleMutexExample.Run();
         // MultipleMutexExample.Run();
         // ReaderWriterLockExample.Run();
-
-        // Concurrent collections.
+        //
+        // // Concurrent collections.
         // ConcurrentDictionaryExample.Run();
         // ConcurrentQueueExample.Run();
         // ConcurrentStackExample.Run();
         // ConcurrentBagExample.Run();
         // BlockingCollectionExample.Run();
-
-        // Task coordination.
+        //
+        // // Task coordination.
         // ContinuationsExample.Run();
         // ChildTaskExample.Run(); // Have option to test different scenarios inside.
         // BarrierExample.Run();
         // CountdownEventExample.Run();
         // ResetEventExample.Run();
         // SemaphoreSlimExample.Run();
-
-        // Parallel loops.
+        //
+        // // Parallel loops.
         // ParallelInvokeForForeachExample.Run();
         // BreakingCancellationsExceptionsExample.Run();
         // ThreadLocalStorageExample.Run();
         // PartitioningExample.Run();
-
-        // Parallel Linq.
+        //
+        // // Parallel Linq.
         // AsParallelAndParallelQueryExample.Run();
         // CancellationAndExceptionsExample.Run();
         // MergeOptionsExample.Run();
